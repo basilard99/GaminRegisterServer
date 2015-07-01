21454a2330fe9ab2576d17351a930d6c23257cd9
Chris Snyder <basilard99@gmail.com>
2015-07-01 12:42:04 -0400

    added get all publishers and updated tests

--- tests/DataServiceTests.js
+++ tests/DataServiceTests.js
@@ -9,16 +9,63 @@ var TEST_CODE = 'TWS';
 var TEST_ISACTIVE = 'false';
 var TEST_DESCRIPTION = 'TestDescription';
 
+var clearNeo = function(done) {
+	db.query('MATCH (n) DELETE (n)', function(err, result) {
+		done();
+	});
+};
+
+var addTestPublisher = function(done) {
+	db.query("CREATE (n:Publisher " +
+			"{ name: '" + TEST_NAME + "', " + 
+			"webSite: '" + TEST_WEBSITE + "', " +
+			"code: '" + TEST_CODE + "', " +
+			"isActive: '" + TEST_ISACTIVE + "', " +
+			"description: '" + TEST_DESCRIPTION + "'})", function(err, result) {
+		if (err) throw err;
+		done();
+	});	
+}
+
 describe('Data Service Tests', function() {
+	
 	describe('saving a publisher', function() {
+		
+		before(function(done) {
+			clearNeo(function() { done(); })
+		});
+		
 		it('should save a publisher', function(done) {
 			
 			var pub = publisherFactory.createPublisher(TEST_NAME, TEST_WEBSITE, TEST_CODE, TEST_ISACTIVE, TEST_DESCRIPTION);
-			var node = dataService.savePublisher(pub, function(node) {
+			dataService.savePublisher(pub, function(node) {
 				assert.ok(node);
 				done();
 			});			
 			
-		})
+		});
+	})
+	
+	describe('getting all publishers', function() {
+		
+		before(function(done) {
+			clearNeo(function() { 
+				addTestPublisher(function() {
+					done();
+				});
+			});
+		});
+		
+		it('should get all publishers', function(done) {
+			
+			var publishers = dataService.getAllPublishers(function(models) {
+				
+				assert.isTrue(models.length > 0);
+				assert.isTrue(models[0].name === TEST_NAME);
+				done();
+				
+			});
+		});
+		
 	})
 })
