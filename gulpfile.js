9bf1749a658ab40c43b829a52474ef5778aba34f
Chris Snyder <basilard99@gmail.com>
2016-01-04 03:08:26 -0500

    converted neo4jManager to use promises

--- gulpfile.js
+++ gulpfile.js
@@ -8,61 +8,59 @@ var nodemon = require('gulp-nodemon');
 var gulpMocha = require('gulp-mocha');
 var env = require('gulp-env');
 var eslint = require('gulp-eslint');
-var neo4jManager = require('./neo4jFunctions')();
+var neo4jManager = require('./neo4jFunctions').create();
 
 gulp.task('dev', function devTask() {
 
 	env({ vars: { ENV: 'test' } });
 
-	neo4jManager.switchToDevelopmentDb();
-
-	var watcher = gulp.watch(['app.js',
+    var watcher = gulp.watch(['app.js',
                               'gulpfile.js',
                               'neo4jFunctions.js',
                               './lib/**/*.js',
                               './tests/**/*.js']);
 
-	watcher.on('change', function actOnChange() {
-		gulp.src(['./lib/**/*.js', './tests/**/*.js'])
-			.pipe(eslint())
-			.pipe(eslint.format());
+    watcher.on('change', function actOnChange() {
+        console.log('-------------CHANGE!');
+        gulp.src(['./lib/**/*.js', './tests/**/*.js'])
+            .pipe(eslint())
+            .pipe(eslint.format());
 
-		gulp.src('tests/unitTests/*.js', { read: false })
-			.pipe(gulpMocha());
-	});
+        gulp.src('tests/unitTests/*.js', { read: false })
+            .pipe(gulpMocha());
+    });
 
-	nodemon({
-		script: 'app.js',
-		ext: 'js',
-		env: {
-			PORT: 8000
-		},
-		ignore: ['./node_modules/**']
-	})
-	.on('restart', function actOnRestart() {
-		console.log('Restarting');
-	})
-	.on('exit', function cleanUp() {
-		console.log('Cleaning up');
-	});
+    nodemon({
+        script: 'app.js',
+        ext: 'js',
+        env: {
+            PORT: 8000
+        },
+        ignore: ['./node_modules/**']
+    })
+    .on('restart', function actOnRestart() {
+        console.log('Restarting');
+    })
+    .on('exit', function cleanUp() {
+        console.log('Cleaning up');
+    });
 
 });
 
 gulp.task('lint', function lintTask() {
-	return gulp.src(['gulpfile.js', './lib/**/*.js', './tests/**/*.js'])
+	return gulp.src(['gulpfile.js', 'neo4jFunctions.js', './lib/**/*.js', './tests/**/*.js'])
                .pipe(eslint())
                .pipe(eslint.format());
 });
 
 gulp.task('integration', function integrationTestTask(done) {
 	env({ vars: { ENV: 'test' } });
-
-	neo4jManager.switchToDevelopmentDb(null, function doneSwitching() {
-		gulp.src('tests/integrationTests/*.js', { read: false })
-			.pipe(gulpMocha());
-		done();
-	});
-
+	neo4jManager.switchToDevelopmentDb(null)
+                .then(function doneSwitching() {
+                    gulp.src('tests/integrationTests/*.js', { read: false })
+                        .pipe(gulpMocha());
+                    done();
+                });
 });
 
 gulp.task('unit', function unitTestTask() {
@@ -71,13 +69,12 @@ gulp.task('unit', function unitTestTask() {
 });
 
 gulp.task('data', function dataTestTask(done) {
-
-	neo4jManager.switchToDevelopmentDb(null, function doneSwitching() {
-		gulp.src('tests/dataTests/*.spec.js', { read: false })
-			.pipe(gulpMocha());
-		done();
-	});
-
+	neo4jManager.switchToDevelopmentDb(null)
+                .then(function doneSwitching() {
+                    gulp.src('tests/dataTests/*.spec.js', { read: false })
+                        .pipe(gulpMocha());
+                    done();
+                });
 });
 
 gulp.task('default', function defaultTask() {
--- neo4jFunctions.js
+++ neo4jFunctions.js
@@ -1,62 +1,74 @@
 'use strict';
 
-var spawn = require('child_process').spawn;
-var monitorctrlc = require('monitorctrlc');
+var Promise = require('bluebird');
 
 var DEFAULT_DEV_DB_NAME = 'test_graph.db';
 var DEFAULT_PROD_DB_NAME = 'prod_graph_db';
 var NEO_4J_MANAGEMENT_HOME = 'C:\\Users\\basil\\Downloads\\neo4j-community-3.0.0-M01-windows\\neo4j-community-3.0.0-M01\\bin\\Neo4j-Management\\';
 
-var neo4jManager = function manager() {
+var ThisObj = function neo4jOperations() {
+};
 
-    function initializeNeo4j() {
+ThisObj.prototype = {
 
-        var child = spawn("powershell.exe", ["-Command", "-"]);
-        child.stderr.on("data",function(data){
-            console.log("Powershell Errors: " + data);
+    switchToDevelopmentDb: function switchToDev(pathToDevelopmentDb) {
+        return new Promise(function switchToDevPromise(resolve, reject) {
+
+            try {
+                var path = pathToDevelopmentDb || DEFAULT_DEV_DB_NAME;
+                var child = initializeNeo4J();
+
+                console.log('--- Resetting to database: ' + path);
+
+                child.stdin.write('Set-Neo4JSetting -ConfigurationFile neo4j-server.properties -Name org.neo4j.server.database.location -Value data/' + path +'\n'); //eslint-disable-line max-len
+                child.stdin.write('Restart-Neo4JServer\n');
+                child.stdin.end();
+
+                setTimeout(function timeoutExceeded() {
+                    console.log('--- Done resetting');
+                    resolve();
+                }, 20000);
+            } catch (err) {
+                reject(err);
+            }
+
         });
-        child.on("exit",function(){
-            console.log("Powershell Script finished");
-        });
-        child.stdin.write("import-module " + NEO_4J_MANAGEMENT_HOME + "Neo4j-Management.psm1\n");
+    },
 
-        return child;
+    switchToProductionDb: function switchToProd(pathToProductionDb) {
 
-    };
-
-    var _child = initializeNeo4j();
-
-    var switchToDevelopmentDb = function switchToDev(pathToDevelopmentDb, done) {
-
-        var path = pathToDevelopmentDb || DEFAULT_DEV_DB_NAME;
+        var path = pathToProductionDb || DEFAULT_PROD_DB_NAME;
+        var child = this.initializeNeo4J();
 
         console.log('--- Resetting to database: ' + path);
 
-        _child.stdin.write('Set-Neo4JSetting -ConfigurationFile neo4j-server.properties -Name org.neo4j.server.database.location -Value data/' + path +'\n');
-        _child.stdin.write('Restart-Neo4JServer\n');
-        _child.stdin.end();
+        child.stdin.write('Set-Neo4JSetting -ConfigurationFile neo4j-server.properties -Name org.neo4j.server.database.location -Value data/' + path); //eslint-disable-line max-len
+        child.stdin.write('estart-Neo4JServer\n');
+        child.stdin.end();
 
         setTimeout(function timeoutExceeded() {
             console.log('--- Done resetting');
             done();
         }, 20000);
-    };
+    }
+};
 
-    var switchToProductionDb = function switchToProd(pathToProductionDb) {
+function initializeNeo4J() {
+    var spawn = require('child_process').spawn;
+    var child = spawn('powershell.exe', ['-Command', '-']);
 
-        var path = pathToDevelopmentDb || DEFAULT_PROD_DB_NAME;
+    child.stderr.on('data', function displayErrors(data) {
+        console.log('Powershell Errors: ' + data);
+    });
 
-        _child.stdin.write("Set-Neo4JSetting -ConfigurationFile neo4j-server.properties -Name org.neo4j.server.database.location -Value data/" + path);
-        _child.stdin.write("Restart-Neo4JServer\n");
-        _child.stdin.end();
+    child.on('exit', function displayExit() {
+        console.log('Powershell Script finished');
+    });
+    child.stdin.write('import-module ' + NEO_4J_MANAGEMENT_HOME + 'Neo4j-Management.psm1\n');
 
-    };
+    return child;
+};
 
-    return {
-        switchToDevelopmentDb: switchToDevelopmentDb,
-        switchToProductionDb: switchToProductionDb
-    };
-
-}
-
-module.exports = neo4jManager;
+module.exports.create = function create() {
+    return new ThisObj();
+};
