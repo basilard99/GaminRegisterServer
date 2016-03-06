892f27d26cd9b0adb0e9b2871b19f20dd70b184e
Chris Snyder <basilard99@gmail.com>
2016-03-06 00:59:58 -0500

    Created value type

---
+++ tests/unitTests/valueTypesTests.js
@@ -0,0 +1,66 @@
+'use strict';
+
+var assert = require('chai').assert;
+var valueTypes = require('../../lib/models/valueTypes.js');
+
+describe.only('NameObj - ', function nameObjTestSuite() {
+
+    it('When created with nothing will throw an exception', function testUndefined() {
+        assert.throws(function assertCheck() {
+            valueTypes.createName();
+        }, 'The name value must be defined and have a value');
+    });
+
+    it('When created with undefined will throw an exception', function testUndefined() {
+        assert.throws(function assertCheck() {
+            valueTypes.createName(undefined);
+        }, 'The name value must be defined and have a value');
+    });
+
+    it('When created with null will throw an exception', function testNull() {
+        assert.throws(function assertCheck() {
+            valueTypes.createName(null);
+        }, 'The name value must be defined and have a value');
+    });
+
+    it('When created with an empty string will throw an exception', function testEmpty() {
+        assert.throws(function assertCheck() {
+            valueTypes.createName('');
+        }, 'The name value must be defined and have a value');
+    });
+
+    it('When created with a 1 letter string will throw an exception', function testTooShort1() {
+        assert.throws(function assertCheck() {
+            valueTypes.createName('a');
+        }, 'The name value must contain at least 3 characters');
+    });
+
+    it('When created with a 2 letter string will throw an exception', function testTooShort2() {
+        assert.throws(function assertCheck() {
+            valueTypes.createName('ab');
+        }, 'The name value must contain at least 3 characters');
+    });
+
+    it('When created with a 31 letter string will throw an exception', function testTooLong() {
+        assert.throws(function assertCheck() {
+            valueTypes.createName('abcdegfhijklmnopqrstuvwxyz12345');
+        }, 'The name value cannot contain more than 3 characters');
+    });
+
+    it('When created with a 3 letter string will allow reading of name', function testMinimumAllowed() {
+        var cut = valueTypes.createName('abc');
+        assert.equal(cut.name, 'abc');
+    });
+
+    it('When created with a 30 letter string will allow reading of name', function testMaximumAllowed() {
+        var cut = valueTypes.createName('abcdegfhijklmnopqrstuvwxyz1234');
+        assert.equal(cut.name, 'abcdegfhijklmnopqrstuvwxyz1234');
+    });
+
+    it('Should not allow modification of object', function testInternalAccess() {
+        var cut = valueTypes.createName('abc');
+        assert.throws(function assertCheck() {
+            cut.name = 'test';
+        }, TypeError);
+    });
+});
