a5b9256b47cf65429152a4c7c5767a4b88f943a6
Chris Snyder <basilard99@gmail.com>
2016-03-11 20:35:24 -0500

    Changed publisher to use value types; had to update tests

---
+++ tests/unitTests/publisher.spec.js
@@ -0,0 +1,31 @@
+'use strict';
+
+var assert = require('chai').assert;
+var publisherFactory = require('../../lib/models/publisher.js');
+
+var TEST_NAME = 'TestName';
+var TEST_NAME2 = 'TestName2';
+var TEST_WEBSITE = 'www.TestWebSite.com';
+var TEST_CODE = 'TWS';
+var TEST_ISACTIVE = false;
+var TEST_DESCRIPTION = 'TestDescription';
+
+describe('The publisher should behave as follows - ', function describe() {
+
+    it ('the factory should create an object matching the creation parameters',
+        function test() {
+            var cut = publisherFactory.createPublisher(TEST_NAME,
+                                                       TEST_WEBSITE,
+                                                       TEST_CODE,
+                                                       TEST_ISACTIVE,
+                                                       TEST_DESCRIPTION
+                      );
+
+            assert.strictEqual(cut.name, TEST_NAME);
+            assert.strictEqual(cut.webSite, TEST_WEBSITE);
+            assert.strictEqual(cut.code, TEST_CODE);
+            assert.strictEqual(cut.isActive, TEST_ISACTIVE);
+            assert.strictEqual(cut.description, TEST_DESCRIPTION);
+        }
+    );
+});
