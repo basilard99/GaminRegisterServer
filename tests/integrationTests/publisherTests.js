d7e32fb95bfd78e9aa27f5cda91b14d2c327a240
Chris Snyder <basilard99@gmail.com>
2016-05-11 14:20:55 -0400

    Changed publisher controller and tests to use promises

--- tests/integrationTests/publisherTests.js
+++ tests/integrationTests/publisherTests.js
@@ -5,55 +5,117 @@ var app = require('../../app.js');
 var request = require('supertest')(app);
 var neo4jManager = require('../.././neo4jFunctions').create();
 
-describe('Publisher Integration Tests', function describe() {
+describe('Publisher Integration Tests', function publisherSuite() {
 
-	beforeEach(function beforeEach(done) {
+	beforeEach(function setupDataForEachTest(done) {
 		neo4jManager.clearNeo4j()
 			.then(function neo4jClearedSuccessfully() {
 				done();
 			}, function neo4jFailedToClear(err) {
-				console.log(err);
 				done();
 			});
 	});
 
-	it('Should allow a publisher to be saved and return a publisher id', function test(done) {
-		var testData = {
-			name: 'Fantasy Flight Games',
-			webSite: 'http://www.fantasyflightgames.com',
-			code: 'FFG',
-			isActive: true
-		};
-
-		request
-			.post('/api/publisherList')
-			.send(testData)
-			.expect(200)
-			.end(function end(err, results) {
-				results.body.should.have.property('id');
-				done();
-			});
-	});
-
-	it('should allow a single publisher to be retrieved', function test(done) {
-		var testData = {
-			name: 'Fantasy Flight Games',
-			webSite: 'http://www.fantasyflightgames.com',
-			code: 'FFG',
-			isActive: true
-		};
-
-		request
-			.post('/api/publisherList')
-			.send(testData)
-			.end(function end(err) {
-				if (err) {
-					should.fail('Unable to post publisher: ' + err.message);
-					done();
-				}
+	describe('Given a valid publisher on the client', function givenAValidPublisherOnClient() {
+		context('When POSTed to a publisher list', function whenPosting() {
+			it('Should receive a 201 (Created) response', function shouldReceive201(done) {
+				var testData = {
+					name: 'Fantasy Flight Games',
+					webSite: 'http://www.fantasyflightgames.com',
+					code: 'FFG',
+					isActive: true
+				};
 
 				request
-					.get('/api/publisherList/' + testData.name)
+					.post('/api/PublisherList/')
+					.send(testData)
+					.expect(201)
+					.end(function end(err, results) {
+						done();
+					});
+			});
+
+			it('Should receive a reference to the created entity', function shouldReceiveReferenceToEntity(done) {
+				var testData = {
+					name: 'Fantasy Flight Games',
+					webSite: 'http://www.fantasyflightgames.com',
+					code: 'FFG',
+					isActive: true
+				};
+
+				request
+					.post('/api/PublisherList/')
+					.send(testData)
+					.expect(201)
+					.end(function end(err, results) {
+						results.text.should.equal('/publisherList/Fantasy%20Flight%20Games');
+						done();
+					});
+			});
+
+			it('Should receive a Location Header to the created entity', function shouldReceiveLocation(done) {
+				var testData = {
+					name: 'Fantasy Flight Games',
+					webSite: 'http://www.fantasyflightgames.com',
+					code: 'FFG',
+					isActive: true
+				};
+
+				request
+					.post('/api/PublisherList/')
+					.send(testData)
+					.expect(201)
+					.expect('Location', '/publisherList/Fantasy%20Flight%20Games')
+					.end(function end(err, results) {
+						done();
+					});
+			});
+		});
+
+		context('When PUT', function whenPutting() {
+			it('Should receive a 201 (Created) response status', function shouldReceive201(done) {
+				var testData = {
+					name: 'Fantasy Flight Games',
+					webSite: 'http://www.fantasyflightgames.com',
+					code: 'FFG',
+					isActive: true
+				};
+
+				request
+					.put('/api/PublisherList/' + testData.name)
+					.send(testData)
+					.expect(201)
+					.end(function end(err, results) {
+						done();
+					});
+			});
+		});
+	});
+
+	describe('Given a publisher exists on the server', function givenPublisherExists() {
+		beforeEach(function createTestData(done) {
+			var testData = {
+				name: 'Fantasy Flight Games',
+				webSite: 'http://www.fantasyflightgames.com',
+				code: 'FFG',
+				isActive: true
+			};
+
+			request
+				.post('/api/publisherList')
+				.send(testData)
+				.end(function end(err) {
+					if (err) {
+						should.fail('Unable to post publisher: ' + err.message);
+					};
+					done();
+				});
+		});
+
+		context('When GETting', function gettingPublisherData() {
+			it('a single publisher should be retrieved', function retrieveSinglePublisher(done) {
+				request
+					.get('/api/publisherList/Fantasy%20Flight%20Games')
 					.expect(200)
 					.end(function end(err, results) {
 						if (err) {
@@ -62,151 +124,15 @@ describe('Publisher Integration Tests', function describe() {
 						}
 
 						results.body.should.have.property('name');
-						should.equal(results.body.name, testData.name);
-						done();
-					});
-			});
-	});
-
-	it('should get all publishers', function test(done) {
-		var testData = {
-			name: 'Fantasy Flight Games',
-			webSite: 'http://www.fantasyflightgames.com',
-			code: 'FFG',
-			isActive: true
-		};
-		var testData2 = {
-			name: 'Wizards of the Coast',
-			webSite: 'http://www.wotc.com',
-			code: 'WOTC',
-			isActive: true };
-
-		request
-			.post('/api/publisherList')
-			.send(testData)
-			.end(function end(err) {
-
-				if (err) {
-					should.fail('Unable to post publisher: ' + err.message);
-					done();
-				}
-
-				request.post('/api/publisherList')
-					.send(testData2)
-					.end(function end(err) {
-
-						if (err) {
-							should.fail('Unable to post publisher: '
-										+ err.message);
+							should.equal(results.body.name, 'Fantasy Flight Games');
 							done();
-						}
-
-						request
-							.get('/api/publisherList')
-							.expect(200)
-							.end(function end(err, results) {
-								if (err) {
-									should.fail(err.message);
-									done();
-								}
-								should.equal(results.body.list.length, 2);
-								done();
-							});
-					});
+						});
 			});
-	});
-
-	it('should allow updating of a publisher via put', function test(done) {
-		var testData = {
-			name: 'Fantasy Flight Games',
-			webSite: 'http://www.fantasyflightgames.com',
-			code: 'FFG',
-			isActive: true
-		};
-
-		request
-			.post('/api/publisherList')
-			.send(testData)
-			.end(function end() {
-				testData.webSite = 'www.TestWebSiteNew.com';
-				request
-					.put('/api/publisherList/' + testData.name)
-					.send(testData)
-					.expect(201)
-					.end(function end(err) {
-						if (err) {
-							should.fail(err.message);
-						}
-						done();
-					});
-			});
-	});
-
-	it('should allow updating of a publisher via patch', function test(done) {
-		var testData = {
-			name: 'Fantasy Flight Games',
-			webSite: 'http://www.fantasyflightgames.com',
-			code: 'FFG',
-			isActive: true
-		};
-
-		request
-			.patch('/api/publishers')
-			.send(testData)
-			.end(function end() {
-				testData.webSite = 'www.TestWebSiteNew.com';
-				request
-					.put('/api/publisherList/' + testData.name)
-					.send(testData)
-					.expect(201)
-					.end(function end(err) {
-						if (err) {
-							should.fail(err.message);
-						}
-						done();
-					});
-			});
-	});
-
-	it('should allow putting a publisher list', function test(done) {
-		var testData = { list: [
-								{
-									name: 'Fantasy Flight Games',
-									webSite: 'http://www.fantasyflightgames.com',
-									code: 'FFG',
-									isActive: true
-								},
-								{
-									name: 'Wizards of the Coast',
-									webSite: 'http://www.wotc.com',
-									code: 'WOTC',
-									isActive: true
-								}
-                               ]
-		};
-
-		request
-			.put('/api/publisherList')
-			.send(testData)
-			.expect(201)
-			.end(function end() {
-				request
-					.get('/api/publisherList')
-					.expect(200)
-					.end(function end(err, results) {
-						if (err) {
-							should.fail(err.message);
-							done();
-						}
-
-						should.equal(results.body.list.length, 2);
-						done();
-					});
-			});
+		});
 	});
 });
 
-after(function after(done) {
+after(function cleanupAfterTests(done) {
 	app.server.close();
 	console.log('shutting down Express');
 	done();
